59e662d6b59db318ccd84ecfa093e5c3ff0c00be
vishesh211 <visheshtiwari211@gmail.com>
2023-04-04 08:59:48 +0530

    feat: created book entity

--- app/src/main/java/com/example/booksnotesapp/data/Book.kt
+++ app/src/main/java/com/example/booksnotesapp/data/Book.kt
@@ -1,3 +1,13 @@
 package com.example.booksnotesapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.booksnotesapp.utils.Constants.BOOK_TABLE
 
+@Entity(tableName = BOOK_TABLE)
+data class Book(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val author: String
+)
