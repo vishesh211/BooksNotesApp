8e2e9e70da080beac9c151b59a45f82023cda0ed
vishesh211 <visheshtiwari211@gmail.com>
2023-04-05 20:48:15 +0530

    feat: created MainActivity.kt

--- app/src/main/java/com/example/booksnotesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/booksnotesapp/presentation/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.booksnotesapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.booksnotesapp.navigation.NavGraph
 import com.example.booksnotesapp.ui.theme.BooksNotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BooksNotesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                NavGraph(navController = rememberNavController())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BooksNotesAppTheme {
-        Greeting("Android")
     }
 }
