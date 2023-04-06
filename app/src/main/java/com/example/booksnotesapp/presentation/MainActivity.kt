6b9eee089aa8c5d5191e008ceb4a69326beca5d4
vishesh211 <visheshtiwari211@gmail.com>
2023-04-06 23:01:53 +0530

    feat: androidEntryPoint annotation

--- app/src/main/java/com/example/booksnotesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/booksnotesapp/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.booksnotesapp.navigation.NavGraph
 import com.example.booksnotesapp.ui.theme.BooksNotesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
